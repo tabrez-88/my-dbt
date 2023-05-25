d9e48b41fd6e20486237107573274f3e289a88bd
tabrez-88 <tabrez.upwork88@gmail.com>
2023-05-25 13:01:01 +0800

    update

--- macros/base64.sql
+++ macros/base64.sql
@@ -1,4 +1,4 @@
 {% macro decode_base64(field) %}
     -- Implementation for PostgreSQL
-    convert_from(decode("`{{ field }}`", 'base64'), 'UTF8')
+    convert_from(decode("{{ field }}", 'base64'), 'UTF8')
 {% endmacro %}
