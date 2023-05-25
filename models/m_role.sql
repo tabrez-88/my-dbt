f77d3af3e4dd67119f3bf98e7f73b9bc41162e8f
tabrez-88 <tabrez.upwork88@gmail.com>
2023-05-25 22:44:55 +0800

    update

--- models/m_role.sql
+++ models/m_role.sql
@@ -2,7 +2,7 @@
 
 SELECT 
     ROW_NUMBER() OVER () as id,
-    r."NAME" as name,
+    {{ decode_base64(r."NAME") }} as name,
     'No description available' as description,
     FALSE as is_disabled
 FROM {{ ref('role') }} AS r 
