58709df93c9c3a1c8c1ecff82526ba29943b0822
tabrez-88 <tabrez.upwork88@gmail.com>
2023-05-26 11:30:59 +0800

    update

--- models/m_client.sql
+++ models/m_client.sql
@@ -3,35 +3,35 @@
 WITH final_client_mapping AS (
     SELECT 
         ROW_NUMBER() OVER () as id,
-        {{ decode_base64(fc.ENCODEDKEY) }} as external_id,
-        ms.id as created_by,
-        ms.id as last_modified_by,
-        fc.BIRTHDATE as date_of_birth,
-        fc.CREATIONDATE as submittedon_date,
-        {{ decode_base64(fc.EMAILADDRESS) }} as email_address,
-        {{ decode_base64(fc.FIRSTNAME) }} as firstname,
-        {{ decode_base64(fc.MIDDLENAME) }} as middlename,
-        {{ decode_base64(fc.LASTNAME) }} as lastname,
-        cv.id as gender_cv_id,
-        fc.LASTMODIFIEDDATE as last_modified_on_utc,
-        fc.LASTMODIFIEDDATE as updated_on,
-        {{ decode_base64(fc.ID) }} as account_no,
-        {{ decode_base64(fc.MOBILEPHONE1) }} as mobile_no,
-        mo.id as office_id,
+        {{ decode_base64(final_client.ENCODEDKEY) }} as external_id,
+        m_staff.id as created_by,
+        m_staff.id as last_modified_by,
+        final_client.BIRTHDATE as date_of_birth,
+        final_client.CREATIONDATE as submittedon_date,
+        {{ decode_base64(final_client.EMAILADDRESS) }} as email_address,
+        {{ decode_base64(final_client.FIRSTNAME) }} as firstname,
+        {{ decode_base64(final_client.MIDDLENAME) }} as middlename,
+        {{ decode_base64(final_client.LASTNAME) }} as lastname,
+        m_code_value.id as gender_cv_id,
+        final_client.LASTMODIFIEDDATE as last_modified_on_utc,
+        final_client.LASTMODIFIEDDATE as updated_on,
+        {{ decode_base64(final_client.ID) }} as account_no,
+        {{ decode_base64(final_client.MOBILEPHONE1) }} as mobile_no,
+        m_office.id as office_id,
         CASE 
-            WHEN fc.STATE = 'ACTIVE' THEN 300
-            WHEN fc.STATE = 'EXITED' THEN 600
-            WHEN fc.STATE = 'REJECTED' THEN 700
-            WHEN fc.STATE = 'PENDING_APPROVAL' THEN 100
-            WHEN fc.STATE = 'BLACKLISTED' THEN 500
+            WHEN final_client.STATE = 'ACTIVE' THEN 300
+            WHEN final_client.STATE = 'EXITED' THEN 600
+            WHEN final_client.STATE = 'REJECTED' THEN 700
+            WHEN final_client.STATE = 'PENDING_APPROVAL' THEN 100
+            WHEN final_client.STATE = 'BLACKLISTED' THEN 500
             ELSE 0
         END as status_enum,
-        fc.ACTIVATIONDATE as activation_date,
-        fc.CLOSEDDATE as closedon_date
-    FROM {{ ref('final_client') }} AS fc
-    LEFT JOIN {{ ref('m_staff') }} AS ms ON {{ decode_base64(fc.ASSIGNEDUSERKEY) }} = ms.external_id
-    LEFT JOIN {{ ref('m_code_value') }} AS cv ON {{ decode_base64(fc.GENDER) }} = cv.code_value
-    LEFT JOIN {{ ref('m_office') }} AS mo ON {{ decode_base64(fc.ASSIGNEDBRANCHKEY) }} = mo.external_id
+        final_client.ACTIVATIONDATE as activation_date,
+        final_client.CLOSEDDATE as closedon_date
+    FROM {{ source('your_database', 'final_client') }} AS final_client
+    LEFT JOIN {{ ref('m_staff') }} AS m_staff ON {{ decode_base64(final_client.ASSIGNEDUSERKEY) }} = m_staff.external_id
+    LEFT JOIN {{ ref('m_code_value') }} AS m_code_value ON {{ decode_base64(final_client.GENDER) }} = m_code_value.code_value
+    LEFT JOIN {{ ref('m_office') }} AS m_office ON {{ decode_base64(final_client.ASSIGNEDBRANCHKEY) }} = m_office.external_id
 )
 
 SELECT * FROM final_client_mapping;
