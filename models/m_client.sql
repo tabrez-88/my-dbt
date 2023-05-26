86ed68bbaa6b0aea5bdf569f8f3656bb5771705b
tabrez-88 <tabrez.upwork88@gmail.com>
2023-05-26 18:10:45 +0800

    update

--- models/m_client.sql
+++ models/m_client.sql
@@ -1,39 +1,35 @@
-{{
-    config(
-        materialized = 'table'
-    )
-}}
+{{ config(materialized='table') }}
 
-WITH base AS (
+WITH final_client_mapping AS (
     SELECT 
         ROW_NUMBER() OVER () as id,
-        decode_base64_or_text(fcl."encodedkey") as external_id,
-        decode_base64_or_text(fcl."assigneduserkey") as created_by,
-        decode_base64_or_text(fcl."assigneduserkey")as last_modified_by,
-        fcl."birthdate" as date_of_birth,
-        fcl."creationdate" as submittedon_date,
-        fcl."emailaddress" as email_address,
-        decode_base64_or_text(fcl."firstname") as firstname,
-        fcl."middlename" as middlename,
-        decode_base64_or_text(fcl."lastname") as lastname,
-        (SELECT id FROM m_code_value WHERE code_value = decode_base64_or_text(fcl."gender"))  as gender_cv_id,
-        fcl."lastmodifieddate" as last_modified_on_utc,
-        fcl."lastmodifieddate" as updated_on,
-        decode_base64_or_text(fcl."profilepicturekey") as image_id,
-        decode_base64_or_text(fcl."profilesignaturekey") as signature_id,
-        decode_base64_or_text(fcl."ID") as account_no,
-        decode_base64_or_text(fcl."mobilephone1") as mobile_no,
-        decode_base64_or_text(fcl."assignedbranchkey") as office_id,
+        {{ decode_base64(fcl.ENCODEDKEY) }} as external_id,
+        m_staff.id as created_by,
+        m_staff.id as last_modified_by,
+        fcl.BIRTHDATE as date_of_birth,
+        fcl.CREATIONDATE as submittedon_date,
+        fcl.EMAILADDRESS as email_address,
+        {{ decode_base64(fcl.FIRSTNAME) }} as firstname,
+        fcl.MIDDLENAME as middlename,
+        {{ decode_base64(fcl.LASTNAME) }} as lastname,
+       (SELECT id FROM m_code_value WHERE code_value = decode_base64_or_text(fcl."gender"))  as gender_cv_id,
+        fcl.LASTMODIFIEDDATE as last_modified_on_utc,
+        fcl.LASTMODIFIEDDATE as updated_on,
+        fcl.MOBILEPHONE1 as mobile_no,
+        m_office.id as office_id,
         CASE 
-            WHEN decode_base64_or_text(fcl."STATE") = 'ACTIVE' THEN 300 
-            WHEN decode_base64_or_text(fcl."STATE") = 'EXITED' THEN 600
-            WHEN decode_base64_or_text(fcl."STATE") = 'REJECTED' THEN 700
-            WHEN decode_base64_or_text(fcl."STATE") = 'PENDING_APPROVAL' THEN 100
-            WHEN decode_base64_or_text(fcl."STATE") = 'BLACKLISTED' THEN 400
-            ELSE 0 
+            WHEN fcl.STATE = 'ACTIVE' THEN 300
+            WHEN fcl.STATE = 'EXITED' THEN 600
+            WHEN fcl.STATE = 'REJECTED' THEN 700
+            WHEN fcl.STATE = 'PENDING_APPROVAL' THEN 100
+            WHEN fcl.STATE = 'BLACKLISTED' THEN 500
+            ELSE 0
         END as status_enum,
-        fcl."activationdate" as activation_date,
-        fcl."closeddate" as closedon_date
-        FROM {{ ref('final_client') }} as fcl
+        fcl.ACTIVATIONDATE as activation_date,
+        fcl.CLOSEDDATE as closedon_date
+    FROM {{ ref('fcl') }} AS fcl
+    LEFT JOIN {{ ref('m_staff') }} AS m_staff ON {{ decode_base64(fcl.ASSIGNEDUSERKEY) }} = m_staff.external_id
+    LEFT JOIN {{ ref('m_office') }} AS m_office ON {{ decode_base64(fcl.ASSIGNEDBRANCHKEY) }} = m_office.external_id
 )
-SELECT * FROM base
+
+SELECT * FROM final_client_mapping;
