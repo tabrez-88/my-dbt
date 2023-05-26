446b5ea7cd11459288399ab7950a9bbd6deaeed2
tabrez-88 <tabrez.upwork88@gmail.com>
2023-05-26 19:49:39 +0800

    update

--- models/m_appuser.sql
+++ models/m_appuser.sql
@@ -1,17 +1,28 @@
 {{ config(materialized='table') }}
 
-WITH user_office AS (
+WITH user_decoded AS (
     SELECT 
-        {{ decode_base64(u.ENCODEDKEY) }} AS user_external_id,
         u."ID" AS id,
-        {{ decode_base64(u.USERNAME) }} AS username,
-        {{ decode_base64(u.FIRSTNAME) }} AS firstname,
-        {{ decode_base64(u.LASTNAME) }} AS lastname,
-        {{ decode_base64(u."PASSWORD") }} AS password,
-        {{ decode_base64(u.EMAIL) }} AS email,
-        s.office_id
+        {{ decode_base64('u.ENCODEDKEY') }} AS user_external_id,
+        {{ decode_base64('u.USERNAME') }} AS username,
+        {{ decode_base64('u.FIRSTNAME') }} AS firstname,
+        {{ decode_base64('u.LASTNAME') }} AS lastname,
+        {{ decode_base64('u."PASSWORD"') }} AS password,
+        {{ decode_base64('u.EMAIL') }} AS email
     FROM {{ ref('user') }} AS u
-    LEFT JOIN {{ ref('m_staff') }} AS s ON u.ENCODEDKEY = s.external_id
+),
+user_office AS (
+    SELECT 
+        ud.user_external_id,
+        ud.id,
+        ud.username,
+        ud.firstname,
+        ud.lastname,
+        ud.password,
+        ud.email,
+        s.office_id
+    FROM user_decoded AS ud
+    LEFT JOIN {{ ref('m_staff') }} AS s ON ud.user_external_id = s.external_id
 )
 
 SELECT 
--- models/m_client.sql
+++ models/m_client.sql
@@ -1,35 +1,54 @@
 {{ config(materialized='table') }}
 
-WITH final_client_mapping AS (
+WITH decoded_client AS (
     SELECT 
-        ROW_NUMBER() OVER () as id,
-        {{ decode_base64(fcl.ENCODEDKEY) }} as external_id,
-        m_staff.id as created_by,
-        m_staff.id as last_modified_by,
         fcl.BIRTHDATE as date_of_birth,
         fcl.CREATIONDATE as submittedon_date,
-        {{ decode_base64(fcl.EMAILADDRESS) }} as email_address,
-        {{ decode_base64(fcl.FIRSTNAME) }} as firstname,
-        {{ decode_base64(fcl.MIDDLENAME) }} as middlename,
-        {{ decode_base64(fcl.LASTNAME) }} as lastname,
-        (SELECT id FROM m_code_value WHERE code_value = {{ decode_base64("'gender'") }})  as gender_cv_id,
         fcl.LASTMODIFIEDDATE as last_modified_on_utc,
-        fcl.LASTMODIFIEDDATE as updated_on,
-        {{ decode_base64(fcl.MOBILEPHONE1) }} as mobile_no,
+        fcl.ACTIVATIONDATE as activation_date,
+        fcl.CLOSEDDATE as closedon_date,
+        {{ decode_base64('fcl.ENCODEDKEY') }} as external_id,
+        {{ decode_base64('fcl.EMAILADDRESS') }} as email_address,
+        {{ decode_base64('fcl.FIRSTNAME') }} as firstname,
+        fcl.MIDDLENAME as middlename,
+        {{ decode_base64('fcl.LASTNAME') }} as lastname,
+        {{ decode_base64('fcl.MOBILEPHONE1') }} as mobile_no,
+        {{ decode_base64('fcl.ASSIGNEDUSERKEY') }} as assigned_user_key,
+        {{ decode_base64('fcl.ASSIGNEDBRANCHKEY') }} as assigned_branch_key,
+        {{ decode_base64('fcl."gender"') }} as gender,
+        {{ decode_base64('fcl."STATE"') }} as state
+    FROM {{ ref('final_client') }} AS fcl
+),
+final_client_mapping AS (
+    SELECT 
+        ROW_NUMBER() OVER () as id,
+        dc.external_id,
+        m_staff.id as created_by,
+        m_staff.id as last_modified_by,
+        dc.date_of_birth,
+        dc.submittedon_date,
+        dc.email_address,
+        dc.firstname,
+        dc.middlename,
+        dc.lastname,
+        (SELECT id FROM m_code_value WHERE code_value = dc.gender)  as gender_cv_id,
+        dc.last_modified_on_utc,
+        dc.last_modified_on_utc as updated_on,
+        dc.mobile_no,
         m_office.id as office_id,
         CASE 
-            WHEN {{ decode_base64("'STATE'") }} = 'ACTIVE' THEN 300
-            WHEN {{ decode_base64("'STATE'") }} = 'EXITED' THEN 600
-            WHEN {{ decode_base64("'STATE'") }} = 'REJECTED' THEN 700
-            WHEN {{ decode_base64("'STATE'") }} = 'PENDING_APPROVAL' THEN 100
-            WHEN {{ decode_base64("'STATE'") }} = 'BLACKLISTED' THEN 500
+            WHEN dc.state = 'ACTIVE' THEN 300
+            WHEN dc.state = 'EXITED' THEN 600
+            WHEN dc.state = 'REJECTED' THEN 700
+            WHEN dc.state = 'PENDING_APPROVAL' THEN 100
+            WHEN dc.state = 'BLACKLISTED' THEN 500
             ELSE 0
         END as status_enum,
-        fcl.ACTIVATIONDATE as activation_date,
-        fcl.CLOSEDDATE as closedon_date
-    FROM {{ ref('final_client') }} AS fcl
-    LEFT JOIN {{ ref('m_staff') }} AS m_staff ON {{ decode_base64(fcl.ASSIGNEDUSERKEY) }} = m_staff.external_id
-    LEFT JOIN {{ ref('m_office') }} AS m_office ON {{ decode_base64(fcl.ASSIGNEDBRANCHKEY) }} = m_office.external_id
+        dc.activation_date,
+        dc.closedon_date
+    FROM decoded_client AS dc
+    LEFT JOIN {{ ref('m_staff') }} AS m_staff ON dc.assigned_user_key = m_staff.external_id
+    LEFT JOIN {{ ref('m_office') }} AS m_office ON dc.assigned_branch_key = m_office.external_id
 )
 
 SELECT * FROM final_client_mapping
