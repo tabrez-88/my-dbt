62db727517c63b7d29230f7521cb4adb813d841d
tabrez-88 <tabrez.upwork88@gmail.com>
2023-05-26 11:51:19 +0800

    update

--- models/m_client.sql
+++ models/m_client.sql
@@ -7,23 +7,23 @@
 WITH base AS (
     SELECT 
         ROW_NUMBER() OVER () as id,
-        decode_base64(fcl."ENCODEDKEY") as external_id,
-        decode_base64(fcl."ASSIGNEDUSERKEY") as created_by,
-        decode_base64(fcl."ASSIGNEDUSERKEY") as last_modified_by,
-        fcl."BIRTHDATE" as date_of_birth,
-        fcl."CREATIONDATE" as submittedon_date,
-        decode_base64(fcl."EMAILADDRESS") as email_address,
-        decode_base64(fcl."FIRSTNAME") as firstname,
-        decode_base64(fcl."MIDDLENAME") as middlename,
-        decode_base64(fcl."LASTNAME") as lastname,
-        (SELECT id FROM m_code_value WHERE code_value = decode_base64(fcl."GENDER")) as gender_cv_id,
-        fcl."LASTMODIFIEDDATE" as last_modified_on_utc,
-        fcl."LASTMODIFIEDDATE" as updated_on,
-        decode_base64(fcl."PROFILEPICTUREKEY") as image_id,
-        decode_base64(fcl."PROFILESIGNATUREKEY") as image_id,
+        decode_base64(fcl."encodedkey") as external_id,
+        decode_base64(fcl."assigneduserkey") as created_by,
+        decode_base64(fcl."assigneduserkey") as last_modified_by,
+        fcl."birthdate" as date_of_birth,
+        fcl."creationdate" as submittedon_date,
+        decode_base64(fcl."emailaddress") as email_address,
+        decode_base64(fcl."firstname") as firstname,
+        decode_base64(fcl."middlename") as middlename,
+        decode_base64(fcl."lastname") as lastname,
+        (SELECT id FROM m_code_value WHERE code_value = decode_base64(fcl."gender")) as gender_cv_id,
+        fcl."lastmodifieddate" as last_modified_on_utc,
+        fcl."lastmodifieddate" as updated_on,
+        decode_base64(fcl."profilepicturekey") as image_id,
+        decode_base64(fcl."profilesignaturekey") as image_id,
         decode_base64(fcl."ID") as account_no,
-        decode_base64(fcl."MOBILEPHONE1") as mobile_no,
-        decode_base64(fcl."ASSIGNEDBRANCHKEY") as office_id,
+        decode_base64(fcl."mobilephone1") as mobile_no,
+        decode_base64(fcl."assignedbranchkey") as office_id,
         CASE 
             WHEN decode_base64(fcl."STATE") = 'ACTIVE' THEN 300 
             WHEN decode_base64(fcl."STATE") = 'EXITED' THEN 600
@@ -32,8 +32,8 @@ WITH base AS (
             WHEN decode_base64(fcl."STATE") = 'BLACKLISTED' THEN 400
             ELSE 0 
         END as status_enum,
-        fcl."ACTIVATIONDATE" as activation_date,
-        fcl."CLOSEDDATE" as closedon_date
+        fcl."activationdate" as activation_date,
+        fcl."closeddate" as closedon_date
         FROM {{ ref('final_client') }} as fcl
 )
 SELECT * FROM base
