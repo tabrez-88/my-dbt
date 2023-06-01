a9267fd0c0645a65be5e7cd6d36b8743692f3136
tabrez-88 <tabrez.upwork88@gmail.com>
2023-06-02 00:44:49 +0800

    update

--- models/m_client_view.sql
+++ models/m_client_view.sql
@@ -12,7 +12,7 @@ WITH base AS (
         {{ decode_base64("assigneduserkey") }} as last_modified_by,
         "birthdate" as date_of_birth,
         "creationdate" as submittedon_date,
-        {{ decode_base64("emailaddress") }} as email_address,
+        /*{{ decode_base64("emailaddress") }} as email_address,
         {{ decode_base64("firstname") }} as firstname,
         {{ decode_base64("lastname") }} as lastname,
         (SELECT id FROM m_code_value WHERE code_value = {{ decode_base64("gender") }})  as gender_cv_id,
@@ -30,7 +30,7 @@ WITH base AS (
             WHEN {{ decode_base64("STATE") }} = 'PENDING_APPROVAL' THEN 100
             WHEN {{ decode_base64("STATE") }} = 'BLACKLISTED' THEN 400
             ELSE 0 
-        END as status_enum,
+        END as status_enum,*/
         "activationdate" as activation_date,
         "closeddate" as closedon_date
         FROM {{ ref('final_client') }}
