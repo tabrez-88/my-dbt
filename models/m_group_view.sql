26950ce0fcde14d1aff373a92bbef25e0033aa6d
tabrez-88 <tabrez.upwork88@gmail.com>
2023-06-02 01:25:47 +0800

    update

--- models/m_group_view.sql
+++ models/m_group_view.sql
@@ -10,7 +10,7 @@ SELECT
     staff.id as staff_id,
     "creationdate" as submittedon_date,
     {{ decode_base64("groupname") }} as display_name,
-    {{ decode_base64("ID") }} as account_no,
+    ID as account_no,
     300 as status_enum, -- default value for status_enum in your PostgreSQL table
     1 as level_id -- assuming some default value for level_id, replace as appropriate
 FROM {{ ref('final_group') }}
