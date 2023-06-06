bc17e876c388daa413fb6c3055160a4029fa30be
tabrez-88 <tabrez.upwork88@gmail.com>
2023-06-06 14:21:11 +0800

    update

--- models/m_loan_repayment_schedule_view.sql
+++ models/m_loan_repayment_schedule_view.sql
@@ -1,14 +1,27 @@
--- models/m_loan_repayment_schedule.sql
-SELECT
-    ROW_NUMBER() OVER (ORDER BY ENCODEDKEY) as id,  -- Generate a new ID as it's not provided in source
-    -- Need to handle loan_id as it's not provided in source
-    CREATIONDATE as fromdate,
-    DUEDATE as duedate,
-    -- Need to handle installment as it's not provided in source
-    PRINCIPALDUE as principal_amount,
-    PRINCIPALPAID as principal_completed_derived,
-    -- Need to handle principal_writtenoff_derived as it's not provided in source
-    INTERESTDUE as interest_amount,
-    INTERESTPAID as interest_completed_derived,
-    -- Need to handle rest of the fields as they are not provided in source
-FROM {{ source('public', 'repayment') }}
+WITH decoded_repayment AS (
+    SELECT
+        ROW_NUMBER() OVER (ORDER BY ENCODEDKEY) as id,
+        {{ decode_base64("parentaccountkey") }} AS parentaccountkey,
+        CREATIONDATE as fromdate,
+        DUEDATE as duedate,
+        PRINCIPALDUE as principal_amount,
+        PRINCIPALPAID as principal_completed_derived,
+        INTERESTDUE as interest_amount,
+        INTERESTPAID as interest_completed_derived
+    FROM 'repayment'
+),
+repayment_with_loan_id AS (
+    SELECT 
+        dr.id,
+        mv_loan.account_no AS loan_id,
+        dr.fromdate,
+        dr.duedate,
+        dr.principal_amount,
+        dr.principal_completed_derived,
+        dr.interest_amount,
+        dr.interest_completed_derived
+    FROM decoded_repayment AS dr
+    LEFT JOIN {{ ref('m_loan_view') }} AS mv_loan ON dr.parentaccountkey = mv_loan.external_id
+)
+
+SELECT * FROM repayment_with_loan_id
