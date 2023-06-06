c26bf744360fe451659964f19148fc1bb621f811
tabrez-88 <tabrez.upwork88@gmail.com>
2023-06-06 13:20:14 +0800

    update

--- models/m_loan_transaction_view.sql
+++ models/m_loan_transaction_view.sql
@@ -2,24 +2,32 @@
     CONVERT_FROM(DECODE({{ field }}, 'base64'), 'utf-8')
 {% endmacro %}
 
-WITH decoded_loantransaction AS (
+WITH type_mapping AS (
+    SELECT * 
+    FROM (VALUES 
+        ('DISBURSEMENT', 'DISBURSMENT'),
+        ('REPAYMENT', 'REPAYMENT'),
+        ('WRITEOFF', 'WRITE_OFF'),
+        ('INITIATE_TRANSFER', 'TRANSFER')
+    ) AS v(original, mapped)
+),
+decoded_loantransaction AS (
     SELECT
         {{ decode_base64("encodedkey") }} AS external_id,
         AMOUNT AS amount,
         BALANCE AS outstanding_loan_balance_derived,
-        /*{{ decode_base64("details_encodedkey_oid") }} AS payment_detail_id,-- this needs to be fixed not same type as payment_detail*/
         creationdate AS created_date,
-        /*{{ decode_base64("TYPE") }} AS transaction_type,  -- can't find same transaction type*/
+        tm.mapped AS transaction_type, 
         ENTRYDATE AS transaction_date,
         PRINCIPALAMOUNT AS principal_portion_derived,
         INTERESTAMOUNT AS interest_portion_derived,
         FEESAMOUNT AS fee_charges_portion_derived,
         PENALTYAMOUNT AS penalty_charges_portion_derived,
         transactionid as id,
-        /*PRINCIPALBALANCE AS outstanding_loan_balance_derived, -- not sure maybe ?*/
         {{ decode_base64("branchkey") }} AS branch_key,
         {{ decode_base64("userkey") }} AS user_key
-    FROM {{ ref('final_loantransaction') }}
+    FROM {{ ref('final_loantransaction') }} ft
+    LEFT JOIN type_mapping tm ON {{ decode_base64("ft.TYPE") }} = tm.original
 ),
 loan_transactions AS (
     SELECT 
@@ -28,10 +36,9 @@ loan_transactions AS (
         dlt.amount,
         dlt.outstanding_loan_balance_derived,
         mv_office.id AS office_id,
-        /*dlt.payment_detail_id,*/
         dlt.created_date,
         mv_loan.account_no AS loan_id,
-       /* get_transaction_type_enum(dlt.transaction_type) AS transaction_type_enum,*/
+        re.enum_id AS transaction_type_enum,
         mv_staff.id AS created_by,
         dlt.transaction_date,
         dlt.principal_portion_derived,
@@ -42,6 +49,7 @@ loan_transactions AS (
     LEFT JOIN {{ ref('m_office_view') }} AS mv_office ON dlt.branch_key = mv_office.external_id
     LEFT JOIN {{ ref('m_staff_view') }} AS mv_staff ON dlt.user_key = mv_staff.external_id
     LEFT JOIN {{ ref('m_loan_view') }} AS mv_loan ON dlt.external_id = mv_loan.external_id
+    LEFT JOIN public.r_enum_value AS re ON (dlt.transaction_type = re.enum_message_property AND re.enum_name = 'loan_transaction_type_enum')
 )
 
 SELECT 
