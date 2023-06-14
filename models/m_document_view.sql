4d8dc49d4656549715b7f733461966fa84da1a18
tabrez-88 <tabrez.upwork88@gmail.com>
2023-06-14 14:15:50 +0800

    update

---
+++ models/m_document_view.sql
@@ -0,0 +1,28 @@
+WITH decoded_document AS (
+    SELECT
+        {{ decode_base64("encodedkey") }} AS encodedkey,
+        {{ decode_base64("LOCATION") }} AS "LOCATION",
+        filesize,
+        creationdate,
+        lastmodifieddate,
+        {{ decode_base64("NAME") }} AS "NAME",
+        documentholderkey,
+        createdbyuserkey,
+        {{ decode_base64("originalfilename") }} AS originalfilename,
+        {{ decode_base64("description") }} AS description,
+        documentholdertype,
+        "ID",
+        {{ decode_base64("TYPE") }} AS "TYPE"
+    FROM {{ ref('document') }}
+)
+SELECT
+    "ID" AS id,
+    parent_entity_id,
+    "NAME" AS name,
+    "originalfilename" AS file_name,
+    filesize AS size,
+    "TYPE" AS type,
+    "description" AS description,
+    "LOCATION" AS location,
+    NULL AS storage_type_enum
+FROM decoded_document;
