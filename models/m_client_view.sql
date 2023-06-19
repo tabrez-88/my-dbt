f9808bdc80c88d03851af39bc0682699d5b35395
tabrez-88 <tabrez.upwork88@gmail.com>
2023-06-19 22:46:34 +0800

    update

--- models/m_client_view.sql
+++ models/m_client_view.sql
@@ -8,21 +8,17 @@ WITH base AS (
     SELECT 
         ROW_NUMBER() OVER () as id,
         {{ decode_base64("encodedkey") }} as external_id,
-        {{ decode_base64("assigneduserkey") }} as created_by,
-        {{ decode_base64("assigneduserkey") }} as last_modified_by,
+        {{ decode_base64("assigneduserkey") }} as assigneduserkey,
         "birthdate" as date_of_birth,
         "creationdate" as submittedon_date,
-        /*{{ decode_base64("emailaddress") }} as email_address,*/
         {{ decode_base64("firstname") }} as firstname,
         {{ decode_base64("lastname") }} as lastname,
         (SELECT id FROM m_code_value WHERE code_value = {{ decode_base64("gender") }})  as gender_cv_id,
         "lastmodifieddate" as last_modified_on_utc,
         "lastmodifieddate" as updated_on,
-        /*{{ decode_base64("profilepicturekey") }} as image_id,
-        {{ decode_base64("profilesignaturekey") }} as signature_id,*/
         "ID" as account_no,
         {{ decode_base64("mobilephone1") }} as mobile_no,
-         o.id as office_id,
+        o.id as office_id,
         CASE 
             WHEN {{ decode_base64("STATE") }} = 'ACTIVE' THEN 300 
             WHEN {{ decode_base64("STATE") }} = 'EXITED' THEN 600
@@ -33,7 +29,12 @@ WITH base AS (
         END as status_enum,
         "activationdate" as activation_date,
         "closeddate" as closedon_date
-        FROM {{ ref('final_client') }} c
-        LEFT JOIN m_office_view o ON o.external_id = c.assignedbranchkey
+    FROM {{ ref('final_client') }} c
+    LEFT JOIN m_office_view o ON o.external_id = c.assignedbranchkey
+), 
+m_client as (
+    SELECT  b.*, s1.id as created_by,s1.id as last_modified_by 
+    from base b 
+    join m_staff_view s1 on b.assigneduserkey = s1.external_id -- corrected join condition
 )
-SELECT * FROM base
+SELECT * FROM m_client
