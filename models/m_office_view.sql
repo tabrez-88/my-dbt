1f0a5c899b679ccdcc1a98d85b58606e8373da5f
tabrez-88 <tabrez.upwork88@gmail.com>
2023-06-19 19:26:21 +0800

    update

--- models/m_office_view.sql
+++ models/m_office_view.sql
@@ -1,16 +1,17 @@
 WITH source AS (
     SELECT
-        {{ decode_base64("encodedkey")}} as external_id,
+        convert_from(decode("encodedkey", 'base64'), 'UTF8') as external_id,
         "ID" as id,
-        {{ decode_base64("NAME") }} AS name,
+        convert_from(decode("NAME", 'base64'), 'UTF8') AS name,
         CREATIONDATE as opening_date
     FROM branch
 )
 
 SELECT
     id::int8 as id,
+    CAST(NULL AS int8) as parent_id,
     NULL as hierarchy,
     external_id,
     name,
     opening_date
-FROM source
+FROM source;
