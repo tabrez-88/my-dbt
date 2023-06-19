321f61cf6d599709be839734bb303ce3760b0f11
tabrez-88 <tabrez.upwork88@gmail.com>
2023-06-19 21:55:58 +0800

    update

--- models/m_client_view.sql
+++ models/m_client_view.sql
@@ -4,36 +4,53 @@
     )
 }}
 
-WITH base AS (
+WITH final_client AS (
     SELECT 
-        ROW_NUMBER() OVER () as id,
-        c.encodedkey as external_id,
-        s1.id as created_by,
-        s2.id as last_modified_by,
-        "birthdate" as date_of_birth,
-        "creationdate" as submittedon_date,
-        /*{{ decode_base64("emailaddress") }} as email_address,*/
-        {{ decode_base64("firstname") }} as firstname,
-        {{ decode_base64("lastname") }} as lastname,
-        (SELECT id FROM m_code_value WHERE code_value = {{ decode_base64("gender") }}) as gender_cv_id,
-        "lastmodifieddate" as last_modified_on_utc,
-        "lastmodifieddate" as updated_on,
-        /*{{ decode_base64("profilepicturekey") }} as image_id,
-        {{ decode_base64("profilesignaturekey") }} as signature_id,*/
-        "ID" as account_no,
-        {{ decode_base64("mobilephone1") }} as mobile_no,
-        o.id as office_id,
+        {{ decode_base64("encodedkey") }} AS encodedkey,
+        {{ decode_base64("assigneduserkey") }} AS assigneduserkey,
+        {{ decode_base64("assignedbranchkey") }} AS assignedbranchkey,
+        {{ decode_base64("firstname") }} AS firstname,
+        {{ decode_base64("lastname") }} AS lastname,
+        {{ decode_base64("STATE") }} AS state,
+        {{ decode_base64("birthdate") }} AS birthdate,
+        {{ decode_base64("creationdate") }} AS creationdate,
+        {{ decode_base64("mobilephone1") }} AS mobilephone1,
+        {{ decode_base64("ID") }} AS ID,
+        {{ decode_base64("lastmodifieddate") }} AS lastmodifieddate,
+        {{ decode_base64("activationdate") }} AS activationdate,
+        {{ decode_base64("closeddate") }} AS closeddate
+    FROM {{ ref('m_client_view') }}
+),
+base AS (
+    SELECT 
+        ROW_NUMBER() OVER () AS id,
+        encodedkey AS external_id,
+        s1.id AS created_by,
+        s2.id AS last_modified_by,
+        birthdate AS date_of_birth,
+        creationdate AS submittedon_date,
+        /*{{ decode_base64("emailaddress") }} AS email_address,*/
+        firstname,
+        lastname,
+        (SELECT id FROM m_code_value WHERE code_value = {{ decode_base64("gender_cv_id") }}) AS gender_cv_id,
+        lastmodifieddate AS last_modified_on_utc,
+        lastmodifieddate AS updated_on,
+        /*{{ decode_base64("profilepicturekey") }} AS image_id,
+        {{ decode_base64("profilesignaturekey") }} AS signature_id,*/
+        ID AS account_no,
+        mobilephone1 AS mobile_no,
+        o.id AS office_id,
         CASE 
-            WHEN {{ decode_base64("STATE") }} = 'ACTIVE' THEN 300 
-            WHEN {{ decode_base64("STATE") }} = 'EXITED' THEN 600
-            WHEN {{ decode_base64("STATE") }} = 'REJECTED' THEN 700
-            WHEN {{ decode_base64("STATE") }} = 'PENDING_APPROVAL' THEN 100
-            WHEN {{ decode_base64("STATE") }} = 'BLACKLISTED' THEN 400
+            WHEN state = 'ACTIVE' THEN 300 
+            WHEN state = 'EXITED' THEN 600
+            WHEN state = 'REJECTED' THEN 700
+            WHEN state = 'PENDING_APPROVAL' THEN 100
+            WHEN state = 'BLACKLISTED' THEN 400
             ELSE 0 
-        END as status_enum,
-        "activationdate" as activation_date,
-        "closeddate" as closedon_date
-    FROM {{ ref('final_client') }} c
+        END AS status_enum,
+        activationdate AS activation_date,
+        closeddate AS closedon_date
+    FROM final_client c
     LEFT JOIN m_staff_view s1 ON s1.external_id = c.assigneduserkey
     LEFT JOIN m_staff_view s2 ON s2.external_id = c.assigneduserkey
     LEFT JOIN m_office_view o ON o.external_id = c.assignedbranchkey
