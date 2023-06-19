1debec9aca669d5212a60ed7e1e130a709635cb5
tabrez-88 <tabrez.upwork88@gmail.com>
2023-06-19 20:53:50 +0800

    update

--- models/m_staff_view.sql
+++ models/m_staff_view.sql
@@ -30,18 +30,18 @@ user_with_decoded_keys AS (
 SELECT 
     u."ID" AS id,
     CASE WHEN u.ISCREDITOFFICER = '1' THEN TRUE ELSE FALSE END AS is_loan_officer,
-    r.organisational_role_enum,
     bo.office_id,
     u.decoded_firstname AS firstname,
     u.decoded_lastname AS lastname,
     u.decoded_username AS display_name,
     u.decoded_mobile_no AS mobile_no,
-    u.decoded_email AS email_address,
     u.decoded_external_id as external_id,
+    r.organisational_role_enum,
     CAST(NULL AS int8) AS organisational_role_parent_staff_id,
     u.decoded_is_active AS is_active,
     CAST(u.CREATIONDATE AS date) AS joining_date,
-    CAST(NULL AS int8) AS image_id  -- Assuming there's no equivalent field in the source table
+    CAST(NULL AS int8) AS image_id,  -- Assuming there's no equivalent field in the source table
+    u.decoded_email AS email_address
 
 FROM user_with_decoded_keys AS u
 LEFT JOIN branch_office AS bo ON u.decoded_assignedbranchkey = bo.office_external_id
