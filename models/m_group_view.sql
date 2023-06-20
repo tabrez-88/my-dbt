6966cefc71b3d36b1075941dd4bad7c1a37fb628
tabrez-88 <tabrez.upwork88@gmail.com>
2023-06-20 22:12:47 +0800

    update

--- models/m_group_view.sql
+++ models/m_group_view.sql
@@ -13,7 +13,7 @@ SELECT
     {{ decode_base64("groupname") }} as display_name,
     "ID" as account_no,
     300 as status_enum, -- default value for status_enum in your PostgreSQL table
-    1 as level_id -- assuming some default value for level_id, replace as appropriate
+    2 as level_id -- level_name Group
 FROM {{ ref('final_group') }}
 LEFT JOIN {{ ref('m_office_view') }} as office
 ON {{ decode_base64("assignedbranchkey") }} = office.external_id
