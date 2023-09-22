5e5a435ab032bcefee3d6cd327630e1436fe02c5
tabrez-fiter <tabrez@fiter.io>
2023-09-22 22:08:43 +0800

    update for loan transaction

--- models/m_loan_transaction_view.sql
+++ models/m_loan_transaction_view.sql
@@ -11,23 +11,39 @@ WITH type_mapping AS (
         ('INITIATE_TRANSFER', 'TRANSFER')
     ) AS v(original, mapped)
 ),
+mv_office AS (
+   
+
+    SELECT id,external_id
+    FROM m_office
+),
+mv_staff AS (
+
+    SELECT id,external_id
+    FROM m_staff
+),
+mv_loan AS (
+
+    SELECT id,external_id
+    FROM m_loan
+),
 decoded_loantransaction AS (
     SELECT
-        {{ decode_base64("encodedkey") }} AS external_id,
+        "encodedkey" AS external_id,
         AMOUNT AS amount,
         BALANCE AS outstanding_loan_balance_derived,
-        {{ decode_base64("parentaccountkey") }} AS parent_id,
+        "parentaccountkey" AS parent_id,
         creationdate AS created_date,
-        {{ decode_base64("TYPE") }} AS transaction_type_raw,
+        "TYPE" AS transaction_type_raw,
         ENTRYDATE AS transaction_date,
         PRINCIPALAMOUNT AS principal_portion_derived,
         INTERESTAMOUNT AS interest_portion_derived,
         FEESAMOUNT AS fee_charges_portion_derived,
         PENALTYAMOUNT AS penalty_charges_portion_derived,
         transactionid as id,
-        {{ decode_base64("branchkey") }} AS branch_key,
-        {{ decode_base64("userkey") }} AS user_key
-    FROM {{ ref('final_loantransaction') }}
+        "branchkey" AS branch_key,
+        "userkey" AS user_key
+    FROM final_loantransaction
 ),
 loan_transactions AS (
     SELECT 
@@ -40,8 +56,8 @@ loan_transactions AS (
         mv_loan.id AS loan_id,
         CASE 
             WHEN dlt.transaction_type_raw = 'BRANCH_CHANGED' THEN 3
-            WHEN dlt.transaction_type_raw = 'DEFERRED_INTEREST_APPLIED' THEN 11
-            WHEN dlt.transaction_type_raw = 'DEFERRED_INTEREST_APPLIED_ADJUSTMENT' THEN 11
+            WHEN dlt.transaction_type_raw = 'DEFERRED_INTEREST_APPLIED' THEN 19
+            WHEN dlt.transaction_type_raw = 'DEFERRED_INTEREST_APPLIED_ADJUSTMENT' THEN 19
             WHEN dlt.transaction_type_raw = 'DEFERRED_INTEREST_PAID' THEN 2
             WHEN dlt.transaction_type_raw = 'DEFERRED_INTEREST_PAID_ADJUSTMENT' THEN 10
             WHEN dlt.transaction_type_raw = 'DISBURSMENT' THEN 1
@@ -50,11 +66,11 @@ loan_transactions AS (
             WHEN dlt.transaction_type_raw = 'FEE_ADJUSTMENT' THEN 10
             WHEN dlt.transaction_type_raw = 'FEE_CHARGED' THEN 10
             WHEN dlt.transaction_type_raw = 'IMPORT' THEN 1
-            WHEN dlt.transaction_type_raw = 'INTEREST_APPLIED' THEN 11
-            WHEN dlt.transaction_type_raw = 'INTEREST_APPLIED_ADJUSTMENT' THEN 11
+            WHEN dlt.transaction_type_raw = 'INTEREST_APPLIED' THEN 19
+            WHEN dlt.transaction_type_raw = 'INTEREST_APPLIED_ADJUSTMENT' THEN 19
             WHEN dlt.transaction_type_raw = 'INTEREST_DUE_REDUCED' THEN 4
-            WHEN dlt.transaction_type_raw = 'INTEREST_LOCKED' THEN 11
-            WHEN dlt.transaction_type_raw = 'INTEREST_UNLOCKED' THEN 11
+            WHEN dlt.transaction_type_raw = 'INTEREST_LOCKED' THEN 19
+            WHEN dlt.transaction_type_raw = 'INTEREST_UNLOCKED' THEN 19
             WHEN dlt.transaction_type_raw = 'PENALTIES_DUE_REDUCED' THEN 9
             WHEN dlt.transaction_type_raw = 'PENALTY_ADJUSTMENT' THEN 9
             WHEN dlt.transaction_type_raw = 'PENALTY_APPLIED' THEN 10
@@ -70,15 +86,16 @@ loan_transactions AS (
         dlt.principal_portion_derived,
         dlt.interest_portion_derived,
         dlt.fee_charges_portion_derived,
-        dlt.penalty_charges_portion_derived
+        dlt.penalty_charges_portion_derived,
+        mv_loan.external_id as loan_external_id
     FROM decoded_loantransaction AS dlt
     LEFT JOIN type_mapping tm ON dlt.transaction_type_raw = tm.original
-    LEFT JOIN {{ ref('m_office_view') }} AS mv_office ON dlt.branch_key = mv_office.external_id
-    LEFT JOIN {{ ref('m_staff_view') }} AS mv_staff ON dlt.user_key = mv_staff.external_id
-    LEFT JOIN {{ ref('m_loan_view') }} AS mv_loan ON dlt.parent_id = mv_loan.external_id
+    LEFT JOIN  mv_office ON dlt.branch_key = mv_office.external_id
+    LEFT JOIN  mv_staff ON dlt.user_key = mv_staff.external_id
+    LEFT JOIN  mv_loan ON dlt.parent_id = mv_loan.external_id
 )
 
 SELECT 
     lt.*
 FROM loan_transactions AS lt
-LEFT JOIN {{ ref('m_loan_view') }} AS mv_loan ON lt.external_id = mv_loan.external_id
+LEFT JOIN  mv_loan ON lt.external_id = mv_loan.external_id
--- models/r_enum_value.sql
+++
@@ -1,4 +0,0 @@
-{{ config(materialized='table') }}
-
-SELECT *
-FROM "public"."r_enum_value"
